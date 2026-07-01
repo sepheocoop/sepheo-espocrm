66c5da47cc02f0c61ddb29bbda649cdbb1080645
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2026-07-01 23:04:36 +0100

    .dir-locals.el - emacs config
    Enable PrettierJS in these various modes, within this project.

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+((js-mode . ((eval . (prettier-mode t))))
+ (typescript-mode . ((eval . (prettier-mode t))))
+ (php-mode . ((eval . (prettier-mode t))))
+ (yaml-mode . ((eval . (prettier-mode t)))))
